2ff929839a1551215e449a801e688bfc5b863426
Okami <64143203@g.cmru.ac.th>
2024-09-06 03:28:41 +0700

    firstcomit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const metadata = {
+  title: "Dashboard | Pos Sai Jai",
+  description: "Welcome to Pos Sai Jai, your online.",
+};
+export default function Dashboard() {
+  return (
+    <div>
+
+    </div>
+    
+  );
+};
